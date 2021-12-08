bb8f52cc434d6611807745583e7fc637ba47c5ea
Giuseppe <giuseppe@adriasonline.it>
2021-12-08 17:01:42 +0100

    aggiunto esempio di package esterno e widget statefull

--- la_mia_prima_app/lib/main.dart
+++ la_mia_prima_app/lib/main.dart
@@ -10,6 +10,7 @@ class LaMiaPrimaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
     return MaterialApp(
       title: 'La mia prima App',
       theme: ThemeData(
@@ -19,9 +20,33 @@ class LaMiaPrimaApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Benvenuti al Corso di Flutter'),
         ),
+        // body: RandomWords(),
         body: const Center(
-          child: Text('Mi chiamo Giuseppe Trisciuoglio e sono il vostro professore di Flutter.'),
+            child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+              'Mi chiamo Giuseppe Trisciuoglio e sono il vostro professore di Flutter.'),
+        )),
+      ),
+    );
+  }
+}
+
+class LaMiaSecondaApp extends StatelessWidget {
+  const LaMiaSecondaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'La mia seconda App',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Benvenuti al Corso di Flutter'),
         ),
+        body: RandomSuggestions(),
       ),
     );
   }
@@ -36,6 +61,41 @@ class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Center(child: Text(wordPair.asPascalCase));
+  }
+}
+
+class RandomSuggestions extends StatefulWidget {
+  @override
+  _RandomSuggestionsState createState() => _RandomSuggestionsState();
+}
+
+class _RandomSuggestionsState extends State<RandomSuggestions> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider();
+          /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
